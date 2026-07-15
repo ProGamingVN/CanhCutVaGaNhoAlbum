3dbf700dcd6b0a2e5ce8690faf8ff49168f38576
ProGamingVN <tienhm118@gmail.com>
2026-07-15 13:09:40 +0700

    Add files via upload

---
+++ ANH/api/get-photos.js
@@ -0,0 +1,30 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+module.exports = async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).send("Method Not Allowed");
+  }
+
+  try {
+    const result = await cloudinary.search
+      .expression("folder:album")
+      .with_field("context")
+      .sort_by("created_at", "desc")
+      .max_results(100)
+      .execute();
+
+    return res.status(200).json({
+      resources: result.resources,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message,
+    });
+  }
+};
